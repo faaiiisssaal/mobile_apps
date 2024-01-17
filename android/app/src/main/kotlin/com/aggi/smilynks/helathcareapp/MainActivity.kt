c3d68bbb2772283a61a8d859e6cbd6cf9eb288a1
Faisal Setiadi <faisalsetiadi71@gmail.com>
2024-01-17 16:20:35 +0700

    LAST COMMIT 17/01

--- android/app/src/main/kotlin/com/aggi/smilynks/helathcareapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/aggi/smilynks/helathcareapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.aggi.smilynks.helathcareapp
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
