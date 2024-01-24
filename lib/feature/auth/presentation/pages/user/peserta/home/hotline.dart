d1f189f1fd45857c667a1f7393a086a896e87c72
Faisal Setiadi <faisalsetiadi71@gmail.com>
2024-01-24 16:47:28 +0700

    LAST COMMIT 24/01

--- lib/feature/auth/presentation/pages/user/peserta/home/hotline.dart
+++ lib/feature/auth/presentation/pages/user/peserta/home/hotline.dart
@@ -4,7 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart' as launcher;
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class HotlinePage extends StatelessWidget {
   const HotlinePage({super.key});
